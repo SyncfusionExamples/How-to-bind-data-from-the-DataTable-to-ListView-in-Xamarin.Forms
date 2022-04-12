dc861b7d29b85a9e496fb6113386fca28bec6a94
lakshmi.natarajan <lakshmi.natarajan@syncfusion.com>
2022-04-12 14:42:16 +0530

    Updated the sample to latest version based on guidelines

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ViewModel.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Syncfusion.ListView.XForms;
+using Xamarin.Forms.Internals;
+using System.Data;
+
+namespace ListViewXamarin
+{
+    [Preserve(AllMembers = true)]
+    public class ListViewGroupingViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<object> contactsInfo;
+        private Random random = new Random();
+        public DataTable dt;
+        #endregion
+
+        #region Constructor
+
+        public ListViewGroupingViewModel()
+        {
+            GenerateSource(100);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<object> ContactsInfo
+        {
+            get { return contactsInfo; }
+            set { this.contactsInfo = value; }
+        }
+
+        #endregion
+
+        #region ItemSource
+
+        public void GenerateSource(int count)
+        {
+            contactsInfo = new ObservableCollection<object>();
+
+            dt = new DataTable("Student");
+            dt.Columns.Add("ContactID", typeof(Int32));
+            dt.Columns.Add("ContactName", typeof(string));
+            dt.Columns.Add("ContactType", typeof(string));
+            dt.Columns.Add("ContactNumber", typeof(string));
+
+            //Data  
+            for (int i = 0; i < count; i++)
+            {
+                dt.Rows.Add(i, CustomerNames[i], contactType[random.Next(0, 5)], random.Next(100, 400).ToString() + "-" + random.Next(500, 800).ToString() + "-" + random.Next(1000, 2000).ToString());
+            }
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                contactsInfo.Add(dt.Rows[i]);
+            }
+        }
+
+        string[] contactType = new string[]
+        {
+            "HOME",
+            "WORK",
+            "MOBILE",
+            "OTHER",
+            "BUSINESS"
+        };
+
+        string[] CustomerNames = new string[]
+        {
+            "Kyle",
+            "Gina",
+            "Irene",
+            "Katie",
+            "Michael",
+            "Oscar",
+            "Ralph",
+            "Torrey",
+            "William",
+            "Bill",
+            "Daniel",
+            "Frank",
+            "Brenda",
+            "Danielle",
+            "Fiona",
+            "Howard",
+            "Jack",
+            "Larry",
+            "Holly",
+            "Jennifer",
+            "Liz",
+            "Pete",
+            "Steve",
+            "Vince",
+            "Zeke",
+            "Aiden",
+            "Jackson",
+            "Mason  ",
+            "Liam   ",
+            "Jacob  ",
+            "Jayden ",
+            "Ethan  ",
+            "Noah   ",
+            "Lucas  ",
+            "Logan  ",
+            "Caleb  ",
+            "Caden  ",
+            "Jack   ",
+            "Ryan   ",
+            "Connor ",
+            "Michael",
+            "Elijah ",
+            "Brayden",
+            "Benjamin",
+            "Nicholas",
+            "Alexander",
+            "William",
+            "Matthew",
+            "James  ",
+            "Landon ",
+            "Nathan ",
+            "Dylan  ",
+            "Evan   ",
+            "Luke   ",
+            "Andrew ",
+            "Gabriel",
+            "Gavin  ",
+            "Joshua ",
+            "Owen   ",
+            "Daniel ",
+            "Carter ",
+            "Tyler  ",
+            "Cameron",
+            "Christian",
+            "Wyatt  ",
+            "Henry  ",
+            "Eli    ",
+            "Joseph ",
+            "Max    ",
+            "Isaac  ",
+            "Samuel ",
+            "Anthony",
+            "Grayson",
+            "Zachary",
+            "David  ",
+            "Christopher",
+            "John   ",
+            "Isaiah ",
+            "Levi   ",
+            "Jonathan",
+            "Oliver ",
+            "Chase  ",
+            "Cooper ",
+            "Tristan",
+            "Colton ",
+            "Austin ",
+            "Colin  ",
+            "Charlie",
+            "Dominic",
+            "Parker ",
+            "Hunter ",
+            "Thomas ",
+            "Alex   ",
+            "Ian    ",
+            "Jordan ",
+            "Cole   ",
+            "Julian ",
+            "Aaron  ",
+            "Carson ",
+            "Miles  ",
+            "Blake  ",
+            "Brody  ",
+            "Adam   ",
+            "Sebastian",
+            "Adrian ",
+            "Nolan  ",
+            "Sean   ",
+            "Riley  ",
+            "Bentley",
+            "Xavier ",
+            "Hayden ",
+            "Jeremiah",
+            "Jason  ",
+            "Jake   ",
+            "Asher  ",
+            "Micah  ",
+            "Jace   ",
+            "Brandon",
+            "Josiah ",
+            "Hudson ",
+            "Nathaniel",
+            "Bryson ",
+            "Ryder  ",
+            "Justin ",
+            "Bryce  ",
+        };
+
+
+        #endregion
+    }
+}
